f242ab2e08d1b06876b6d4063edc09903326fb94
AlexSteiner30 <65250433+AlexSteiner30@users.noreply.github.com>
2022-12-14 18:39:56 +0100

    Lobby

---
+++ Assets/Scripts/Wardrobe.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Wardrobe : MonoBehaviour
+{
+    [SerializeField] private GameObject wardrobe;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            wardrobe.SetActive(true);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            wardrobe.SetActive(false);
+        }
+    }
+}
