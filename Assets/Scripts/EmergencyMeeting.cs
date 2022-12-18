815276ecb4b7baccc55100ebee99b1340c3c68a8
AlexSteiner30 <steiner.alex40@gmail.com>
2022-12-18 19:01:08 +0100

    Emergency Meeting

---
+++ Assets/Scripts/EmergencyMeeting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmergencyMeeting : MonoBehaviour {
+
+	[SerializeField] private GameManager gameManager;
+
+	private void Start()
+	{
+		gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.gameObject.CompareTag("Player"))
+		{
+			gameManager.EnableUseButton();
+		}
+	}
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            gameManager.DisableUseButton();
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    [SerializeField] private UnityEngine.UI.Button m_useButton;
+
+    public void UseButton(GameObject panel)
+    {
+        panel.SetActive(true);
+    }
+
+    public void EnableUseButton()
+    {
+        m_useButton.interactable = true;
+    }
+
+    public void DisableUseButton()
+    {
+        m_useButton.interactable = false;
+    }
+}
