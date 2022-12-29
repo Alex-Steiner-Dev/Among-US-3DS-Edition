82b3bdc5927441c20240b4e1ffddb36bd372b052
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2022-12-29 23:06:48 +0100

    AI Demo

---
+++ Assets/Scripts/A Star/Grid.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Grid : MonoBehaviour {
+    [SerializeField] private AIMoving aiMoving;
+
+    public LayerMask unwalkableMask;
+	public Vector2 gridWorldSize;
+	public float nodeRadius;
+	private Node[,] grid;
+
+
+	float nodeDiameter;
+	int gridSizeX, gridSizeY;
+
+	void Awake() {
+		nodeDiameter = nodeRadius*2;
+		gridSizeX = Mathf.RoundToInt(gridWorldSize.x/nodeDiameter);
+		gridSizeY = Mathf.RoundToInt(gridWorldSize.y/nodeDiameter);
+		CreateGrid();
+	}
+    private void Start()
+    {
+        aiMoving = GetComponentInParent<AIMoving>();
+    }
+
+    void CreateGrid() {
+		grid = new Node[gridSizeX,gridSizeY];
+		Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x/2 - Vector3.forward * gridWorldSize.y/2;
+
+		for (int x = 0; x < gridSizeX; x ++) {
+			for (int y = 0; y < gridSizeY; y ++) {
+				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+				bool walkable = !(Physics.CheckSphere(worldPoint,nodeRadius,unwalkableMask));
+				grid[x,y] = new Node(walkable,worldPoint, x,y);
+			}
+		}
+	}
+
+	public List<Node> GetNeighbours(Node node) {
+		List<Node> neighbours = new List<Node>();
+
+		for (int x = -1; x <= 1; x++) {
+			for (int y = -1; y <= 1; y++) {
+				if (x == 0 && y == 0)
+					continue;
+
+				int checkX = node.gridX + x;
+				int checkY = node.gridY + y;
+
+				if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY) {
+					neighbours.Add(grid[checkX,checkY]);
+				}
+			}
+		}
+
+		return neighbours;
+	}
+	
+
+	public Node NodeFromWorldPoint(Vector3 worldPosition) {
+		float percentX = (worldPosition.x + gridWorldSize.x/2) / gridWorldSize.x;
+		float percentY = (worldPosition.z + gridWorldSize.y/2) / gridWorldSize.y;
+		percentX = Mathf.Clamp01(percentX);
+		percentY = Mathf.Clamp01(percentY);
+
+		int x = Mathf.RoundToInt((gridSizeX-1) * percentX);
+		int y = Mathf.RoundToInt((gridSizeY-1) * percentY);
+		return grid[x,y];
+	}
+
+	public List<Node> path;
+
+	void OnDrawGizmos() 
+	{
+        Gizmos.color = Color.white;
+
+        Gizmos.DrawWireCube(transform.position,new Vector3(gridWorldSize.x, gridWorldSize.y, 1));
+
+		if (grid != null) 
+		{
+			foreach (Node n in grid) 
+			{
+				if(path != null && path.Contains(n))
+				{
+                    Gizmos.color = Color.black;
+
+                    Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
+
+                    aiMoving.Move(n.worldPosition);
+                }
+            }
+		}
+	}
+}
--- Assets/Scripts/AI/AIMoving.cs
+++ Assets/Scripts/AI/AIMoving.cs
@@ -3,5 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AIMoving : MonoBehaviour {
-
+    public void Move(Vector2 pos)
+    {
+        float step = .1f * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, pos, step);
+    }
 }
